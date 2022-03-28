54f29e4b2dd59611932989fe17169be1f569e16b
anusaah <59117564+anusaah@users.noreply.github.com>
2022-03-28 21:12:20 +0545

    product image added

--- raah/website/views.py
+++ raah/website/views.py
@@ -5,7 +5,7 @@ def index(request):
     return render(request, 'index.html', {})
 
 def contact(request):
-    return render(request, 'contact.html', {})
+        return render(request, 'contact.html', {})
 
 def product(request):
     return render(request, 'product.html', {})
