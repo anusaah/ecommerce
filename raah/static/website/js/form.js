661f013dad37300e24da1d65cb5bda23cff1a71b
anusaah <59117564+anusaah@users.noreply.github.com>
2022-03-28 06:53:33 +0545

    templates added

---
+++ raah/static/website/js/form.js
@@ -0,0 +1,4 @@
+const toggleForm = () => {
+    const container = document.querySelector('.account-section .container');
+    container.classList.toggle('active');
+  };
---
+++ raah/website/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
+
+def contact(request):
+    return render(request, 'contact.html', {})
+
+def product(request):
+    return render(request, 'product.html', {})
+
+def about(request):
+    return render(request, 'about.html', {})
+
+def cart(request):
+    return render(request, 'cart.html', {})
